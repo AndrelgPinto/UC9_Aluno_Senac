d24987beeb4ebd0f34d677397e5bc4f72c9e846e
andre.lgpinto <andregomeees0@gmail.com>
2023-03-07 22:19:16 -0300

    aluno_senac

--- UC9_Aluno_Senac/Program.cs
+++ UC9_Aluno_Senac/Program.cs
@@ -1,1 +1,11 @@
-﻿string[,] faltas new string []
+﻿string[,] uc = new string[2, 3];
+int l = 0;
+int c = 0;
+string cadastrar = "S";
+double faltas = 0;
+
+while(cadastrar == "S" || cadastrar == "s")
+{
+    Console.WriteLine("")
+
+}
